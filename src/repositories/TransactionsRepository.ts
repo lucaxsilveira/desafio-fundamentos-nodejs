175ccf2bf966f678e7fe099b2deef10ac3f44cbf
Lucas da Silveira <lucas.hoffmann.lh@gmail.com>
2020-04-14 17:55:48 -0300

    Challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionTDO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public sumByType(type: 'income' | 'outcome'): number {
+    const initialValue = 0;
+    return this.transactions.reduce(
+      (prevValue, currentValue) =>
+        currentValue.type === type
+          ? prevValue + currentValue.value
+          : prevValue + 0,
+      initialValue,
+    );
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumIncome = this.sumByType('income');
+    const sumOutcome = this.sumByType('outcome');
+
+    return {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: TransactionTDO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
